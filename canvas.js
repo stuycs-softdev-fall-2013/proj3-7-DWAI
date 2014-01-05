00ce9a4e70eb6f4c11320ab3d54b4c08e50497af
aleung013 <aleung013@gmail.com>
2014-01-04 22:50:34 -0500

    Show pen on canvas

---
+++ canvas.js
@@ -0,0 +1,29 @@
+var canvasScript = function(){
+    var canvas = document.getElementById("cvs");
+    var ctx=canvas.getContext("2d");
+
+    var pen = function(e){
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	ctx.translate(0,0);
+	ctx.beginPath();
+	ctx.setLineDash([1,1]);
+	ctx.arc(e.x,
+		e.y,
+		5,0,2*Math.PI);//x,y,radius,startAng,endAng
+	ctx.stroke();
+    }
+
+
+    var getMousePos = function(canvas,e){
+	var rect = canvas.getBoundingClientRect();
+	return {
+	    x: e.clientX - rect.left,
+	    y: e.clientY - rect.top
+	};
+    }
+    canvas.addEventListener('mousemove',function(e){
+	var mousePos = getMousePos(canvas,e);
+	pen(e);
+    });
+}();
+
