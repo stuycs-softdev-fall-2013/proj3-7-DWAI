0d7d610fcb545de02ef253e68d6f987eb57909e2
aleung013 <aleung013@gmail.com>
2014-01-28 08:52:25 -0500

    More resize canvas

--- static/js/buttons.js
+++ static/js/buttons.js
@@ -1,6 +1,6 @@
 var buttonsScript = function(){
     
-    var width, height;
+    /*var width, height;
     
     document.getElementById("create").addEventListener("click", moveToCanvas); 
     
@@ -9,11 +9,12 @@ var buttonsScript = function(){
 	height = document.getElementById("height").value;
 	window.location="/canvas";
    }
-
+    */
     document.getElementById("delete").addEventListener("click", deleteCanvas);
 
     var deleteCanvas = function(){
 	window.alert("Are you sure you wanna delete this?")
+    }
 }();
 
 
