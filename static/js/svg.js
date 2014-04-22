5c815cace73613376883dc0abbed25cdadec6c33
aleung013 <aleung013@gmail.com>
2014-04-21 23:54:53 -0400

    Save/load

--- static/js/svg.js
+++ static/js/svg.js
@@ -1,20 +1,9 @@
 var draw = function(){
-    var s = document.getElementById('svg');
-    var namespace = "http://www.w3.org/2000/svg"
-    var pen = document.createElementNS(namespace,"circle");
-    pen.setAttribute('id','pen');
-    pen.setAttribute('cx',0);
-    pen.setAttribute('cy',0);
-    pen.setAttribute('r',30);
-    pen.setAttribute('stroke','#000000');
-    pen.setAttribute('fill','#FFFFFF');
-    s.appendChild(pen);
-    var penDown = false;
-    var currPath;
+    var s,namespace,pen;
+    var penDown,currPath,cpathid;    
     var oldMidPtX,oldMidPtY,midPtX,midPtY;
-    var cpathid = 0;
-    var redoStack = [];
-    var data;
+    var redoStack,data;
+
     var drawPath = function(d){
 	var p = document.createElementNS(namespace,'path');
 	p.setAttribute('id','path' + cpathid);
@@ -25,19 +14,7 @@ var draw = function(){
 	p.setAttribute('stroke-linecap','round');
 	s.appendChild(p);	
     };
-    var movePen = function(e){
-	pen.setAttribute('cx',e.offsetX);
-	pen.setAttribute('cy',e.offsetY);
-	if(penDown){
-	    currPath = document.getElementById('path' + cpathid);
-	    midPtX = e.offsetX;
-	    midPtY = e.offsetY;
-	    currPath.setAttribute('d',currPath.getAttribute('d') + ' Q ' + (oldMidPtX + e.offsetX>>1) + ' ' + (oldMidPtY + e.offsetY>>1) + ' ' + midPtX + ' ' + midPtY + ' ');
-	    
-	    oldMidPtX = midPtX;
-	    oldMidPtY = midPtY;
-	}
-    }
+
     var undo = function(){
 	if(cpathid > 0){
 	    currPath = document.getElementById('path'+(cpathid-1));
@@ -53,24 +30,59 @@ var draw = function(){
 	    cpathid++;
 	}
     }
-    s.addEventListener('mousemove',movePen);
-    s.addEventListener('mousedown',function(e){
-	penDown = true;
-	oldMidPtX = e.offsetX;
-	oldMidPtY = e.offsetY;
-	cpath = 'M ' + oldMidPtX + ' ' + oldMidPtY + ' ';
-	drawPath(cpath);
-    });
-    s.addEventListener('mouseup',function(){
-	penDown = false;
-	cpathid++;
-    });
     var save = function(){
-	return s.innerHTML;
+	var imgdata = s.innerHTML;
+	imgdata = imgdata.substring(imgdata.indexOf('<path'));
+	return imgdata;
+    }
+    var load = function(img){
+	s = document.getElementById('svg');
+	s.innerHTML='Your browser does not support the SVG tag';
+	
+	namespace = "http://www.w3.org/2000/svg"
+	pen = document.createElementNS(namespace,"circle");
+	pen.setAttribute('id','pen');
+	pen.setAttribute('cx',0);
+	pen.setAttribute('cy',0);
+	pen.setAttribute('r',30);
+	pen.setAttribute('stroke','#000000');
+	pen.setAttribute('fill','#FFFFFF');
+	s.appendChild(pen);
+	if(img)
+	    s.innerHTML=s.innerHTML + img;
+	penDown = false;
+	
+	cpathid = document.getElementsByTagName("path").length;
+	s.addEventListener('mousemove',function(e){
+	    pen.setAttribute('cx',e.offsetX);
+	    pen.setAttribute('cy',e.offsetY);
+	    if(penDown){
+		currPath = document.getElementById('path' + cpathid);
+		midPtX = e.offsetX;
+		midPtY = e.offsetY;
+		currPath.setAttribute('d',currPath.getAttribute('d') + ' Q ' + (oldMidPtX + e.offsetX>>1) + ' ' + (oldMidPtY + e.offsetY>>1) + ' ' + midPtX + ' ' + midPtY + ' ');
+		
+		oldMidPtX = midPtX;
+		oldMidPtY = midPtY;
+	    }
+	});
+	s.addEventListener('mousedown',function(e){
+	    penDown = true;
+	    oldMidPtX = e.offsetX;
+	    oldMidPtY = e.offsetY;
+	    cpath = 'M ' + oldMidPtX + ' ' + oldMidPtY + ' ';
+	    drawPath(cpath);
+	});
+	s.addEventListener('mouseup',function(){
+	    penDown = false;
+	    cpathid++;
+	});	
     }
     return {
 	undo: undo,
 	redo: redo,
-	save: save
+	save: save,
+	load: load
     };
 }();
+draw.load();
