c474a91199aa2acd7e3058ef2f4667cf121d6f52
Michele <michelelin123@hotmail.com>
2014-04-28 11:59:07 -0400

    changed routes

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
 
 var app = express();
 
@@ -21,8 +21,16 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/', routes);
+//app.use('/users', users);
+app.use(express.session({
+    secret:"secretkey"}));
+
+
+app.get('/', function(req,res){
+    res.render('index', { title: 'Google Paint'});
+});
+
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
